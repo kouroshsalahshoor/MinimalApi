4ac65d5afa84979ed834ec9ec450affaec2f2bc1
kourosh <kourosh23@hotmail.com>
2024-12-10 17:03:15 +0100

    dbcontext

---
+++ MinimalApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Models;
+
+namespace MinimalApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                CategoriesStore.Categories
+            );
+        }
+    }
+}
